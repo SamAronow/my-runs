97f57195da8a3fa58eca3edf60313881daffeddb
Sam Aronow <72639228+SamAronow@users.noreply.github.com>
2024-08-13 23:59:23 -0400

    testing mobile

--- worker.js
+++ worker.js
@@ -1,26 +1,28 @@
-// Listen for messages from the main thread
-self.addEventListener('message', function(event) {
-    const routes = event.data;
+// worker.js
+
+self.onmessage = function(event) {
+    const { routes } = event.data;
     const featureCollection = {
-        "type": "FeatureCollection",
-        "features": []
+      "type": "FeatureCollection",
+      "features": []
     };
-
-    // Process each route
+  
+    // Process routes and build GeoJSON features
     for (let i = 0; i < routes.length; i++) {
-        let route = routes[i];
-        featureCollection.features.push({
-            "type": "Feature",
-            "properties": {
-                "name": route.features[0].start
-            },
-            "geometry": {
-                "type": "LineString",
-                "coordinates": route.features[0].geometry.coordinates[0]
-            }
-        });
+      let route = routes[i];
+      featureCollection.features.push({
+        "type": "Feature",
+        "properties": {
+          "name": route.features[0].start
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": route.features[0].geometry.coordinates[0]
+        }
+      });
     }
-
-    // Send the processed data back to the main thread
+  
+    // Post the featureCollection back to the main thread
     self.postMessage(featureCollection);
-});
+  };
+  
