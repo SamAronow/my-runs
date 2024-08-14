96b17869b36986b48d6073afbee531e59b9bb243
Sam Aronow <72639228+SamAronow@users.noreply.github.com>
2024-08-14 09:57:07 -0400

    testing mobile

--- worker.js
+++ worker.js
@@ -1,19 +1,26 @@
-// worker.js
+// Listen for messages from the main thread
+self.addEventListener('message', function(event) {
+    const routes = event.data;
+    const featureCollection = {
+        "type": "FeatureCollection",
+        "features": []
+    };
 
-onmessage = function(event) {
-    if (event.data.type === 'process') {
-      const chunk = event.data.chunk;
-      const features = chunk.map(route => ({
-        type: 'Feature',
-        properties: {
-          name: route.features[0].start
-        },
-        geometry: {
-          type: 'LineString',
-          coordinates: route.features[0].geometry.coordinates[0]
-        }
-      }));
-      postMessage(features);
+    // Process each route
+    for (let i = 0; i < routes.length; i++) {
+        let route = routes[i];
+        featureCollection.features.push({
+            "type": "Feature",
+            "properties": {
+                "name": route.features[0].start
+            },
+            "geometry": {
+                "type": "LineString",
+                "coordinates": route.features[0].geometry.coordinates[0]
+            }
+        });
     }
-  };
-  
+
+    // Send the processed data back to the main thread
+    self.postMessage(featureCollection);
+});
